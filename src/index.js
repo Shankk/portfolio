28d0c926b573f4836b57b955ac4aee8e8446da5c
Shankk <brianviveiros@hotmail.com>
2023-10-01 05:15:17 -0400

    Brighten Up Site and Improved Bio Section
    Redeisgned Bio Section. Added Dynamic Animations Based on Scrolling. Changed  Color Scheme.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import './styles.css'
 import { generateSocials } from './socials';
+import coolcat from "./images/coolcat.jpg";
 
-window.onscroll = function() {stickyHeader()};
+window.onscroll = function() {pageTransitions()};
 
 let header = document.querySelector(".header");
+let bio = document.querySelector("#bioContainer");
+let work = document.querySelector("#myWork");
+let workTop = document.querySelector("#workTopHalf");
+let workBottom = document.querySelector("#workBottomHalf");
 let footer = document.querySelector(".footer");
 // Get the offset position of the navbar
 let sticky = header.offsetTop;
 
-function stickyHeader() {
+let bioImage = document.querySelector("#bioImg");
+console.log(bioImage)
+bioImage.src = coolcat;
+
+function pageTransitions() {
     if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+        header.classList.add("sticky");
     } else if (window.pageYOffset <= sticky) {
-    header.classList.remove("sticky");
+        header.classList.remove("sticky");
+    }
+
+    if(window.pageYOffset > (bio.offsetTop + bio.offsetHeight)) {
+        bio.classList.remove("moveUp")
+    } else if(window.pageYOffset < (bio.offsetTop + bio.offsetHeight)) {
+        bio.classList.add("moveUp")
+    }
+
+    if(window.pageYOffset > (work.offsetTop - 300)) {
+        workTop.classList.add("moveLeft")
+        workBottom.classList.add("moveRight")
+    } else if (window.pageYOffset < (work.offsetTop - (work.offsetTop-100))) {
+        workTop.classList.remove("moveLeft")
+        workBottom.classList.remove("moveRight")
     }
 }
 
